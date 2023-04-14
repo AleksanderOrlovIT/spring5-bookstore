f5714cec403abfdf4869dc33cfb4f26a29fb0701
Alexandr <Shurlik003@gmail.com>
2023-04-14 20:57:15 +0300

    Created Customer Service and map implementation. Closes #10.

---
+++ src/main/java/com/example/bookstore/service/CustomerService.java
@@ -0,0 +1,6 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.model.Customer;
+
+public interface CustomerService extends CrudService<Customer, Long>{
+}
---
+++ src/main/java/com/example/bookstore/service/map/CustomerServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.bookstore.service.map;
+
+import com.example.bookstore.model.Customer;
+import com.example.bookstore.service.CustomerService;
+
+import java.util.Set;
+
+public class CustomerServiceImpl extends AbstractMapService<Customer, Long> implements CustomerService {
+    @Override
+    public Set<Customer> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return super.save(customer);
+    }
+
+    @Override
+    public void delete(Customer customer) {
+        super.delete(customer);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
