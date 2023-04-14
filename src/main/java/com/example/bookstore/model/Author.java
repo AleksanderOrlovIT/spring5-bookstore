081c85576afea512d143c7c417e466f6e460de16
Alexandr <Shurlik003@gmail.com>
2023-04-14 19:29:52 +0300

    Created Author class. Closes #3.

--- src/main/java/com/example/bookstore/model/Author.java
+++ src/main/java/com/example/bookstore/model/Author.java
@@ -1,9 +1,25 @@
 package com.example.bookstore.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "authors")
 public class Author extends BaseEntity{
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
 }
