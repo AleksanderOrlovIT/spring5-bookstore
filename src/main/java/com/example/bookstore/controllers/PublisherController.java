9bdec0948307d30ee8c07bd220e08bbd806c8d66
Alexandr <Shurlik003@gmail.com>
2023-04-15 16:18:28 +0300

    Created Publisher show functionality. Closes #26.

--- src/main/java/com/example/bookstore/controllers/PublisherController.java
+++ src/main/java/com/example/bookstore/controllers/PublisherController.java
@@ -3,6 +3,7 @@ package com.example.bookstore.controllers;
 import com.example.bookstore.service.PublisherService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +18,11 @@ public class PublisherController {
     public String getPublishers(Model model){
         model.addAttribute("publishers", publisherService.findAll());
         return "publisher/index";
+    }
+
+    @RequestMapping("/publisher/{id}/show")
+    public String showById(@PathVariable String id, Model model){
+        model.addAttribute("publisher", publisherService.findById(Long.valueOf(id)));
+        return "publisher/show";
     }
 }
