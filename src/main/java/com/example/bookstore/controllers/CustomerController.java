a29f8ea669486da5096503bc014a191e336ed2fa
Alexandr <Shurlik003@gmail.com>
2023-04-15 17:42:23 +0300

    Created Customer show Functionality. Closes #29.

--- src/main/java/com/example/bookstore/controllers/CustomerController.java
+++ src/main/java/com/example/bookstore/controllers/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.bookstore.controllers;
 import com.example.bookstore.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +18,11 @@ public class CustomerController {
     public String getCustomers(Model model){
         model.addAttribute("customers", customerService.findAll());
         return "customer/index";
+    }
+
+    @RequestMapping("/customer/{id}/show")
+    public String getCustomerById(@PathVariable String id, Model model){
+        model.addAttribute("customer", customerService.findById(Long.valueOf(id)));
+        return "customer/show";
     }
 }
