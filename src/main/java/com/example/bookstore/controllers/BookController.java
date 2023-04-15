aa789d65d237abbaeae916032c4cc477d879e259
Alexandr <Shurlik003@gmail.com>
2023-04-15 19:27:17 +0300

    Created book new functionality. Closes #19.

--- src/main/java/com/example/bookstore/controllers/BookController.java
+++ src/main/java/com/example/bookstore/controllers/BookController.java
@@ -1,13 +1,19 @@
 package com.example.bookstore.controllers;
 
+import com.example.bookstore.model.Book;
 import com.example.bookstore.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class BookController {
+
+    private static final String bookForm = "/book/bookform";
+
     private final BookService bookService;
 
     public BookController(BookService bookService) {
@@ -24,5 +30,21 @@ public class BookController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("book", bookService.findById(Long.valueOf(id)));
         return "book/show";
+    }
+
+    @GetMapping("/book/new")
+    public String initCreationForm(Model model){
+        model.addAttribute("book", Book.builder().build());
+        return bookForm;
+    }
+
+    @PostMapping("/book/new")
+    public String processCreationForm(@Valid Book book, BindingResult result){
+        if(result.hasErrors()){
+            return bookForm;
+        }else{
+            Book savedBook = bookService.save(book);
+            return "redirect:/book/" + savedBook.getId() + "/show";
+        }
     }
 }
