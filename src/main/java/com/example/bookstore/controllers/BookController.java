9c51f742f825f7c5980953ddcb41849618320422
Alexandr <Shurlik003@gmail.com>
2023-04-15 19:58:27 +0300

    Created book update functionality. Closes #20.

--- src/main/java/com/example/bookstore/controllers/BookController.java
+++ src/main/java/com/example/bookstore/controllers/BookController.java
@@ -1,7 +1,10 @@
 package com.example.bookstore.controllers;
 
 import com.example.bookstore.model.Book;
+import com.example.bookstore.service.AuthorService;
 import com.example.bookstore.service.BookService;
+import com.example.bookstore.service.CustomerService;
+import com.example.bookstore.service.PublisherService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,9 +18,16 @@ public class BookController {
     private static final String bookForm = "/book/bookform";
 
     private final BookService bookService;
+    private final AuthorService authorService;
+    private final PublisherService publisherService;
+    private final CustomerService customerService;
 
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, AuthorService authorService, PublisherService publisherService,
+                          CustomerService customerService) {
         this.bookService = bookService;
+        this.authorService = authorService;
+        this.publisherService = publisherService;
+        this.customerService = customerService;
     }
 
     @RequestMapping({"/books", "/books/show"})
@@ -43,6 +53,24 @@ public class BookController {
         if(result.hasErrors()){
             return bookForm;
         }else{
+            Book savedBook = bookService.save(book);
+            return "redirect:/book/" + savedBook.getId() + "/show";
+        }
+    }
+
+    @GetMapping("/book/{bookId}/update")
+    public String initUpdateBookForm(@PathVariable Long bookId, Model model){
+        model.addAttribute(bookService.findById(bookId));
+        System.out.println(bookService.findById(bookId).getAuthors());
+        return bookForm;
+    }
+
+    @PostMapping("/book/{id}/update")
+    public String processUpdateBookForm(@Valid Book book, BindingResult result, @PathVariable Long id){
+        if(result.hasErrors()){
+            return bookForm;
+        }else{
+            book.setId(id);
             Book savedBook = bookService.save(book);
             return "redirect:/book/" + savedBook.getId() + "/show";
         }
