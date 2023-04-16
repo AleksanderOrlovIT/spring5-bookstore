3135b88e0d867aeaba6cfdb3a489158bb3f3bb6c
Alexandr <Shurlik003@gmail.com>
2023-04-16 19:50:22 +0300

    Create add and show customer book's CLoses #40.

---
+++ src/main/java/com/example/bookstore/controllers/doublecontrollers/CustomerBooksController.java
@@ -0,0 +1,65 @@
+package com.example.bookstore.controllers.doublecontrollers;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.model.Customer;
+import com.example.bookstore.service.BookService;
+import com.example.bookstore.service.CustomerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/customer/{customerId}")
+public class CustomerBooksController {
+    private final static String customerBookForm = "customer/customerbooks/customerbookform";
+
+    private final CustomerService customerService;
+
+    private final BookService bookService;
+
+    private Customer currentCustomer;
+
+    public CustomerBooksController(CustomerService customerService, BookService bookService) {
+        this.customerService = customerService;
+        this.bookService = bookService;
+    }
+
+    @RequestMapping({"/books", "/books/show"})
+    public String getAllCustomersBooks(@PathVariable Long customerId, Model model){
+        currentCustomer = customerService.findById(customerId);
+        model.addAttribute("books", currentCustomer.getBooks());
+        model.addAttribute("customer", currentCustomer);
+        return "customer/customerbooks/index";
+    }
+
+    @GetMapping("/book/new")
+    public String initCreationForm(Model model){
+        model.addAttribute("book", Book.builder().build());
+        return customerBookForm;
+    }
+
+    @PostMapping("/book/new")
+    public String processCreationForm(@Valid Book book, BindingResult result, @PathVariable Long customerId){
+        if(result.hasErrors()){
+            return customerBookForm;
+        }else{
+            Book findBook = bookService.findByName(book.getName());
+            currentCustomer = customerService.findById(customerId);
+            if(findBook != null){
+                book = findBook;
+            }
+            currentCustomer.getBooks().add(book);
+            book.getCustomers().add(currentCustomer);
+
+            customerService.save(currentCustomer);
+            bookService.save(book);
+            return "redirect:/customer/" + customerId + "/books";
+        }
+    }
+}
