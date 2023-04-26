f39d416867a6ebd2791b47b95f147bbb5c7488d1
Alexandr <Shurlik003@gmail.com>
2023-04-26 14:04:21 +0300

    Added customer home page Closes #68.

--- src/main/java/com/example/bookstore/controllers/usercustomercontrollers/UserCustomerController.java
+++ src/main/java/com/example/bookstore/controllers/usercustomercontrollers/UserCustomerController.java
@@ -1,14 +1,20 @@
 package com.example.bookstore.controllers.usercustomercontrollers;
 
 import com.example.bookstore.config.CustomerDetails;
+import com.example.bookstore.model.Customer;
 import com.example.bookstore.service.CustomerService;
+import org.apache.logging.log4j.message.LoggerNameAwareMessage;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserCustomerController {
+
+    private final static String errorPage = "/error/400error";
 
     private final CustomerService customerService;
 
@@ -17,7 +23,13 @@ public class UserCustomerController {
     }
 
     @RequestMapping("/user/{userId}/homePage")
-    public String getCustomerHomePage(){
+    public String getCustomerHomePage(Model model, @PathVariable Long userId){
+        Customer customer = customerService.findById(userId);
+        if(customer == null){
+            model.addAttribute("exception", new Exception("There is no customer with id: " + userId));
+            return errorPage;
+        }
+        model.addAttribute("user", customer);
         return "user/index";
     }
 
